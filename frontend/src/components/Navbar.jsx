37984bc0a4db5370beddcc0083e50dcee85bf078
xauravww <sauravmaheshwari8@gmail.com>
2025-08-21 00:39:17 +0530

    rem linter warning

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState, useContext } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { searchContext } from "../context/SearchContext";
 import { motion, AnimatePresence } from "framer-motion";
 import { Menu, X, Search, ArrowRight } from "lucide-react";
